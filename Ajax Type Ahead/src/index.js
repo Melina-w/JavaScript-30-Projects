9f3ab8d4ad3f5596d1837ae535bb510f776e2e19
MELINA <meluwaigant@gmail.com>
2021-01-26 10:53:40 +1300

    fuction to add commas to the population & css

--- Ajax Type Ahead/src/index.js
+++ Ajax Type Ahead/src/index.js
@@ -14,14 +14,29 @@ function FindMatches(wordToMatch, cities) {
   });
 }
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function displayMatches() {
   const matchArray = FindMatches(this.value, cities);
   const html = matchArray
     .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+
       return `
     <li> 
-    <span class="name"> ${place.city}, ${place.state}</span>
-    <span class="population"> ${place.population} </span> 
+    <span class="name"> ${cityName}, ${stateName}</span>
+    <span class="population">${numberWithCommas(place.population)} </span> 
     </li>
     `;
     })
